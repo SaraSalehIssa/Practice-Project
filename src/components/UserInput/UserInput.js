e359d518b9fd4eab4b6a170889ef1701db1d3fce
SaraSalehIssa <issasalehsara2001@gmail.com>
2023-08-28 13:03:46 +0300

    Managing State

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialUserInput = {
+    'current-savings': 10000,
+    'yearly-contribution': 1200,
+    'expected-return': 7,
+    duration: 10
+};
 
 const UserInput = () => {
+    const [userInput, setUserInput] = useState(initialUserInput);
+
     const submitHandler = (event) => {
         event.preventDefault();
         //.....
@@ -8,13 +17,16 @@ const UserInput = () => {
     };
 
     const resetHandler = () => {
-        //.....
-        console.log('RESET!');
+        setUserInput(initialUserInput);
     };
 
     const inputChangeHandler = (input, value) => {
-        //.....
-        console.log(input, value);
+        setUserInput((prevInput) => {
+            return {
+                ...prevInput,
+                [input]: value
+            };
+        });
     };
 
     return (
@@ -26,6 +38,7 @@ const UserInput = () => {
                         type="number"
                         id="current-savings"
                         onChange={(event) => inputChangeHandler('current-savings', event.target.value)}
+                        value={userInput['current-savings']}
                     />
                 </p>
                 <p>
@@ -34,6 +47,7 @@ const UserInput = () => {
                         type="number"
                         id="yearly-contribution"
                         onChange={(event) => inputChangeHandler('yearly-contribution', event.target.value)}
+                        value={userInput['yearly-contribution']}
                     />
                 </p>
             </div>
@@ -46,6 +60,7 @@ const UserInput = () => {
                         type="number"
                         id="expected-return"
                         onChange={(event) => inputChangeHandler('expected-return', event.target.value)}
+                        value={userInput['expected-return']}
                     />
                 </p>
                 <p>
@@ -54,6 +69,7 @@ const UserInput = () => {
                         type="number"
                         id="duration"
                         onChange={(event) => inputChangeHandler('duration', event.target.value)}
+                        value={userInput['duration']}
                     />
                 </p>
             </div>
